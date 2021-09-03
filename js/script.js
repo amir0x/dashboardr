4a3b0314c9211b12ccc6a518339e141be66ba2ab
amir0x <88179294+amir0x@users.noreply.github.com>
2021-09-03 17:56:54 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let accountHolder = document.querySelector('.account-holder');
+let sideLeft = document.querySelector('.mobile-menu');
+let container = document.querySelector('.container');
+let dropdownLi = document.querySelector('.inner-left-sidebar ul li.dropdown');
+let menuUl = document.querySelector('.inner-left-sidebar ul li.dropdown ul');
+
+accountHolder.addEventListener('click', ()=>{
+    accountHolder.classList.toggle("open");
+})
+sideLeft.addEventListener('click', ()=>{
+    container.classList.toggle("inserted");
+})
+
+dropdownLi.addEventListener('click', ()=>{
+    menuUl.classList.toggle("active");
+})
